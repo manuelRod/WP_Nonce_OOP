55b506ab74fbad700a83db850c53abf010927333
manuelRod <manuel.rodriguez.rosado@gmail.com>
2018-05-14 14:54:33 +0200

    Installing BrainMonkey mockery and downgrading phpunit to make it compatible

--- src/WP_Nonce.php
+++ src/WP_Nonce.php
@@ -2,13 +2,42 @@
 
 namespace manuelRod\WP_Nonce_OOP;
 
+/**
+ * Wraps wp_nonce_* functionality on WP_Nonce Class.
+ *
+ * Class WP_Nonce
+ * @package manuelRod\WP_Nonce_OOP
+ */
 class WP_Nonce {
 
+    /**
+     * Action name. Should give the context to what is taking place.
+     *
+     * @var int|string
+     */
     protected $action;
 
-
-    function __construct($action = -1) {
+    /**
+     * WP_Nonce constructor.
+     * @param int $action
+     */
+    public function __construct($action = -1) {
         $this->action = $action;
     }
+
+
+    /**
+     * Generates and returns a nonce.
+     * The nonce is generated based on the current time, $this->action argument, and the current user ID.
+     *
+     * @return string
+     */
+    public function createNonce() {
+        return wp_create_nonce($this->action);
+    }
+
+
+
+
 
 }
--- tests/src/WP_NonceTest.php
+++ tests/src/WP_NonceTest.php
@@ -1,13 +1,22 @@
 <?php
+use manuelRod\WP_Nonce_OOP\WP_Nonce;
 
 use PHPUnit\Framework\TestCase;
-use manuelRod\WP_Nonce_OOP\WP_Nonce;
+use Brain\Monkey;
 
 class WP_Nonce_Test extends TestCase {
 
-    public function testConstructor() {
-        $generator = new WP_Nonce('action');
-
+    public function setUp() {
+        parent::setUp();
+        Monkey\setUp();
     }
 
+    protected $action = 'test_nonce';
+
+
+
+    public function tearDown() {
+        Monkey\tearDown();
+        parent::tearDown();
+    }
 }
